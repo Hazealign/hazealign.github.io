daae6044e848d6390ebbefb3af52526c67d85312
Travis CI <Realignist@RealignistPro.local>
2016-12-01 16:11:38 +0900

    Deploy to GitHub Pages: eb98ac6a2f4651e62428f286775c3a52c5f8ac56

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 
-ENCRYPTION_LABEL="7fd66f08a613"
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+
+function doCompile {
+  ./compile.sh
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -19,10 +30,13 @@ cd ..
 # Clean out existing contents
 rm -rf out/**/* || exit 0
 
+# Run our compile script
+doCompile
+
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
-git config user.email "hazelee@re.aligni.st"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
@@ -43,7 +57,7 @@ ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key 
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
