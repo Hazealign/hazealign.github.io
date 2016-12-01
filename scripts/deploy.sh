f8d3c238a8e5e04e53e2e7c9c991ae8cfe563d9b
Haze Lee <hazelee@re.aligni.st>
2016-12-01 18:53:28 +0900

    Change ssh to https for Git config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
+git clone https://${GH_TOKEN}@github.com/Hazealign/hazealign.github.io.git out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
@@ -39,15 +39,5 @@ echo "$(ls)"
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
 # Now that we're all set up, we can push.
-git push -f $SSH_REPO HEAD:$TARGET_BRANCH 
+git push $SSH_REPO $TARGET_BRANCH 
