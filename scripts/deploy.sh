fda9fe1b426807ea74d954612049e0d4f1c1b39a
Haze Lee <hazelee@re.aligni.st>
2016-12-01 18:25:23 +0900

    Set Travis Whitelist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -44,8 +44,9 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
-chmod a+x ./../scripts/ssh-add-key.sh
-bash ./../scripts/ssh-add-key.sh deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+send '\r'
 
 # Now that we're all set up, we can push.
-git push -f $SSH_REPO $TARGET_BRANCH 
+git push $SSH_REPO $TARGET_BRANCH 
