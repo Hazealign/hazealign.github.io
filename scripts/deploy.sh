057f9f1c22e388693a0e4ca66554863afbe75e45
Travis CI <hazelee@re.aligni.st>
2017-06-14 06:07:52 +0000

    Deploy to GitHub Pages: 5b58515e99a874b5268768a10eade8f7554111df

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,10 +27,11 @@ git config --global user.name "Travis CI"
 git config --global user.email "hazelee@re.aligni.st"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if [ -z "git diff --exit-code" ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
+
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
